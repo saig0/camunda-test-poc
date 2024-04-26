6d89dd67a49fc76dcb3d261d4fce4748e7de2233
Philipp Ossler <philipp.ossler@gmail.com>
2024-04-26 06:19:31 +0200

    feat: Load Zeebe SNAPSHOT version

--- src/test/java/io/camunda/test/MyTest.java
+++ src/test/java/io/camunda/test/MyTest.java
@@ -6,25 +6,47 @@ import io.camunda.zeebe.client.api.response.ProcessInstanceResult;
 import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
 import io.zeebe.containers.ZeebeContainer;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 public class MyTest {
 
     @Container
-    private final ZeebeContainer zeebeContainer = new ZeebeContainer();
+    private final ZeebeContainer zeebeContainer = new ZeebeContainer(
+            // DockerImageName.parse("camunda/zeebe:8.5.0")
+            DockerImageName.parse("camunda/zeebe:SNAPSHOT")
+    );
 
-    @Test
-    void shouldConnectToZeebe() {
-        // given
+    private ZeebeClient createClient() {
         final ZeebeClient client =
                 ZeebeClient.newClientBuilder()
                         .gatewayAddress(zeebeContainer.getExternalGatewayAddress())
                         .usePlaintext()
                         .build();
+        return client;
+    }
+
+    @Test
+    void testZeebeVersion() {
+        // given
+        final ZeebeClient client = createClient();
+
+        // when
+        String gatewayVersion = client.newTopologyRequest().send().join().getGatewayVersion();
+
+        // then
+        assertThat(gatewayVersion).isEqualTo("8.6.0-SNAPSHOT");
+    }
+
+    @Test
+    void shouldConnectToZeebe() {
+        // given
+        final ZeebeClient client = createClient();
         final BpmnModelInstance process =
                 Bpmn.createExecutableProcess("process").startEvent().endEvent().done();
 
@@ -43,8 +65,9 @@ public class MyTest {
                         .withResult()
                         .send()
                         .join();
-        Assertions.assertThat(processInstanceResult.getProcessDefinitionKey())
+        assertThat(processInstanceResult.getProcessDefinitionKey())
                 .isEqualTo(deploymentEvent.getProcesses().get(0).getProcessDefinitionKey());
     }
+
 
 }
